5660262878a0cfd1cdfbcbdbfb66d1c5faa01f72
Ritika Mandal <ritika.4342@gmail.com>
2020-02-23 09:31:26 +0530

    Class And Objects

--- ShapeTest.java
+++ ShapeTest.java
@@ -1,22 +1,10 @@
-package com.abstract1;
-abstract class Shape1
+class Shape
    {
      abstract void draw();
-	 abstract void shape();
     }
-	  abstract class Point extends Shape1
-	{
-		 void draw()
-		 {
-		
-		   System.out.println("shape is point");
-		 }
-		 
-	}
   class ShapeTest
      {
 	   public static void main(String args[])
 	       {
-			 
 		     }
      }
